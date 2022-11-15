82b1972f60f13f43eadf2ec13012411cae7c6930
Taiye <taiyeajimati@gmail.com>
2022-11-15 22:50:13 +0400

    Last Home Commit

--- hirefreelance/tailwind.config.js
+++ hirefreelance/tailwind.config.js
@@ -18,7 +18,7 @@ module.exports = {
           DEFAULT: '1rem',
           sm: '2rem',
           lg: '4rem',
-          xl: '5rem',
+          xl: '7rem',
           '2xl': '6rem',
         },
         center: true,
